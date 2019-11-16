3d81e0f9de65d5c15e9fefd466e225dbc7633639
daniyalusmani <daniyal.usmani@dreambroker.fi>
2019-11-16 15:39:22 +0200

    full screen and flip camera button

--- src/Qr-scanner.component.js
+++ src/Qr-scanner.component.js
@@ -1,34 +1,44 @@
 import React, { useState } from 'react'
+import {FlipCameraIos} from '@material-ui/icons';
+
 import QrReader from 'react-qr-reader'
 
 export function QrScanner() {
-  const [result, setResult] = useState('No result');
+  const [setResult] = useState('No result');
 
   const handleScan = data => {
     if (data) {
       setResult(data);
       console.log(data);
+      // TODO: go to next screen
       document.localStorage.set('qr-code', data.toString())
     }
   };
+
+  function flipCamera() {
+    alert('Hello!');
+  }
 
   const handleError = err => {
     console.error(err)
   };
 
   return (
-    <div>
+    <section>
+      <section className="Qr-scanner">
       <QrReader
         delay={300}
         onError={handleError}
         onScan={handleScan}
-        style={{ width: '100%' }}
       />
-      <p>{result}</p>
-    </div>
+      </section>
+      <section className="Flip-camera" onClick={flipCamera}>
+        <FlipCameraIos
+          theme="secondary"
+          style={{fontSize: 40, color: "acquawhite"}}></FlipCameraIos>
+      </section>
+    </section>
   );
-
-
 }
 
 export default QrScanner;
