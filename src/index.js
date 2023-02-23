28d58387e0535dd78be99039a627221cf2404084
Abdullah Danyal Saeed <abdullahsaeed92@gmail.com>
2023-02-23 09:43:25 +0200

    First commit

--- src/index.js
+++ src/index.js
@@ -11,4 +11,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // Learn more about service workers: https://bit.ly/CRA-PWA
 // serviceWorker.unregister();
 // serviceWorker.register();
+
+//initial commit
 initializeFirebase();
