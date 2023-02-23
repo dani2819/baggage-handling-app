74de2619fd1cc681b80c8b9e98951553f4872447
Abdullah Danyal Saeed <abdullahsaeed92@gmail.com>
2023-02-23 09:46:57 +0200

    Second commit in child PR

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,5 +5,4 @@ import App from '../components/App';
 it('renders without crashing', () => {
   const div = document.createElement('div');
   ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
 });
